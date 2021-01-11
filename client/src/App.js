27e79264ee234e917094217a39fe42ec253d62d1
idea7654 <idea7654@naver.com>
2021-01-11 23:30:14 +0900

    add axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+const App = () => {
+  const [Title, setTitle] = useState("");
+  useEffect(() => {
+    axios.get("http://localhost:8000/api").then((res) => {
+      //console.log(res);
+      setTitle(res.data[0].title);
+    });
+  }, []);
+  return <div>{Title}</div>;
+};
 
 export default App;
