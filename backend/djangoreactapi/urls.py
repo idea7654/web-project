aa0221a97596b7992aac58d4894131638bfff7f4
KWAK8573 <mill8573@gmail.com>
2021-01-18 22:20:54 +0900

    media repair

--- backend/djangoreactapi/urls.py
+++ backend/djangoreactapi/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -23,4 +25,4 @@ urlpatterns = [
     path("api/", include("api.urls")),
     path("api/", include("post.urls")),
     path("api/auth", include("knox.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
